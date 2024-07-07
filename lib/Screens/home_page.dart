385d5200e025762fd034ac5743a199d98e7e4da6
aBugInTheSystem <ekom338.com@gmail.com>
2024-07-07 12:42:26 +0300

    Refactor

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
 import 'package:medical_appointment/core/styles/app_styles.dart';
@@ -21,49 +20,53 @@ class HomePage extends StatelessWidget {
                 const SizedBox(
                   height: 45,
                 ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text("Hi, Steven",
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.white)),
-                    Stack(
-                      children: [
-                        Container(
-                          width: 48,
-                          height: 48,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(14),
-                            color: const Color(0xFFC4E1FF).withOpacity(0.15),
-                          ),
-                          child: const Icon(IconlyLight.notification, color: Colors.white),
-                        ),
-                        Positioned(
-                          top: 15, // Adjust the position as needed
-                          right: 15, // Adjust the position as needed
-                          child: Container(
-                            width: 8,
-                            height: 8,
-                            decoration: const BoxDecoration(
-                              color: Colors.red,
-                              shape: BoxShape.circle,
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-                    ],
-                  ),
-                ),
+                _welcomeNotification(),
               ],
             ),
           ),
         ],
       ),
     );
+  }
+
+  Container _welcomeNotification() {
+    return Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text("Hi, Steven",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white)),
+                  Stack(
+                    children: [
+                      Container(
+                        width: 48,
+                        height: 48,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(14),
+                          color: const Color(0xFFC4E1FF).withOpacity(0.15),
+                        ),
+                        child: const Icon(IconlyLight.notification, color: Colors.white),
+                      ),
+                      Positioned(
+                        top: 15, // Adjust the position as needed
+                        right: 15, // Adjust the position as needed
+                        child: Container(
+                          width: 8,
+                          height: 8,
+                          decoration: const BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                  ],
+                ),
+              );
   }
 }
