733da6857ab2edc5fb01320869ba78a0d02fb6f4
aBugInTheSystem <ekom338.com@gmail.com>
2024-07-07 07:42:17 +0300

    Colors for the app.

--- lib/core/styles/app_styles.dart
+++ lib/core/styles/app_styles.dart
@@ -2,5 +2,9 @@ import 'dart:ui';
 
 class AppStyles{
   static Color primary = const Color(0xFF51A8FF);
+  static Color black = const Color(0xFF09121C);
   static Color activeBlue = const Color(0xFF4186CC);
+  static Color lightBlue = const Color(0xFFD4E9FF);
+  static Color darkerBlue = const Color(0xFF316599);
+  static Color offShadeBlue = const Color(0xFF142A40);
 }
