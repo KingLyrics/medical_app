c91c72a20fc739befed1822cd38ceba5579c46cf
aBugInTheSystem <ekom338.com@gmail.com>
2024-07-07 08:14:48 +0300

    The work begins.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'core/bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return   MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: "Poppins"),
-      home: const Center(child: Text("Hello")),
+      home:  BottomNavBar(),
     );
   }
 }
