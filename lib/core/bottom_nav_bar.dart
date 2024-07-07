e6bf437e721ca2e1f4ea615c8f5d9889f874ad6e
aBugInTheSystem <ekom338.com@gmail.com>
2024-07-07 11:41:08 +0300

    Piecing the navigation Bar.

--- lib/core/bottom_nav_bar.dart
+++ lib/core/bottom_nav_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
+import 'package:medical_appointment/core/styles/app_styles.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -10,8 +12,19 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return  Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: AppStyles.primary,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(IconlyBold.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(IconlyLight.calendar), label: "Calendar"),
+            BottomNavigationBarItem(icon: Icon(IconlyLight.chat), label: "Chat"),
+            BottomNavigationBarItem(icon: Icon(IconlyLight.user), label: "User")
+          ],
+        ),
     );
   }
 }
