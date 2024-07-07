f1e1f69012faa0a0f2b6e8ca5243bd2711c69061
aBugInTheSystem <ekom338.com@gmail.com>
2024-07-07 07:54:59 +0300

    Fonts. I may not use all so some will be deleted.

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return   MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Text("Hello World"),
+      theme: ThemeData(fontFamily: "Poppins"),
+      home: const Center(child: Text("Hello")),
     );
   }
 }
